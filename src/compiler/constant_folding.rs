80ab078be7cca359598e42c0ec3232dd4203f63b
Leon Rische <leon.rische@me.com>
2018-10-16 20:16:25 +0200

    Refactor constant folding code

--- src/compiler/constant_folding.rs
+++ src/compiler/constant_folding.rs
@@ -1,172 +1,102 @@
-use ::Expression;
-use numbers::Rational;
+use Expression::{Symbol, Integer, Float, List, Rational, self};
+use numbers;
 
 pub fn fold(datum : Expression) -> Expression {
-    match datum.clone() {
-        Expression::List(elems) => {
-            let mut body : Vec<Expression> = elems.into_iter().map(fold).collect();
-            let name = body.remove(0);
-            match name.clone() {
-                Expression::Symbol(ref s) => {
-                    match s.as_ref() {
-                        "neg" => {
-                            if body[0].is_numeric() {
-                                match &body[0] {
-                                    &Expression::Integer(a) => {
-                                        return Expression::Integer(-a)
-                                    },
-                                    &Expression::Float(a) => {
-                                        return Expression::Float(-a)
-                                    },
-                                    _ => {}
-                                }
-                            }
-                        },
-                        "__bin+" => {
-                            if body[0].is_numeric() && body[1].is_numeric() {
-                                match (&body[0], &body[1]) {
-                                    (&Expression::Integer(a), &Expression::Integer(b)) => {
-                                        return Expression::Integer(a + b)
-                                    },
-                                    (&Expression::Float(a), &Expression::Integer(b)) => {
-                                        return Expression::Float(a + (b as f64))
-                                    },
-                                    (&Expression::Integer(a), &Expression::Float(b)) => {
-                                        return Expression::Float((a as f64) + b)
-                                    },
-                                    (&Expression::Float(a), &Expression::Float(b)) => {
-                                        return Expression::Float(a + b)
-                                    }
-                                    _ => {}
-                                }
-                            }
-                        },
-                        "__bin-" => {
-                            if body[0].is_numeric() && body[1].is_numeric() {
-                                match (&body[0], &body[1]) {
-                                    (&Expression::Integer(a), &Expression::Integer(b)) => {
-                                        return Expression::Integer(a - b)
-                                    },
-                                    (&Expression::Float(a), &Expression::Integer(b)) => {
-                                        return Expression::Float(a - (b as f64))
-                                    },
-                                    (&Expression::Integer(a), &Expression::Float(b)) => {
-                                        return Expression::Float((a as f64) - b)
-                                    },
-                                    (&Expression::Float(a), &Expression::Float(b)) => {
-                                        return Expression::Float(a - b)
-                                    }
-                                    _ => {}
-                                }
-                            }
-                        },
-                        "__bin*" => {
-                            if body[0].is_numeric() && body[1].is_numeric() {
-                                match (&body[0], &body[1]) {
-                                    (&Expression::Integer(a), &Expression::Integer(b)) => {
-                                        return Expression::Integer(a * b)
-                                    },
-                                    (&Expression::Float(a), &Expression::Integer(b)) => {
-                                        return Expression::Float(a * (b as f64))
-                                    },
-                                    (&Expression::Integer(a), &Expression::Float(b)) => {
-                                        return Expression::Float((a as f64) * b)
-                                    },
-                                    (&Expression::Float(a), &Expression::Float(b)) => {
-                                        return Expression::Float(a * b)
-                                    }
-                                    _ => {}
-                                }
-                            }
-                        },
-                        "__bin/" => {
-                            if body[0].is_numeric() && body[1].is_numeric() {
-                                match (&body[0], &body[1]) {
-                                    (&Expression::Integer(a), &Expression::Integer(b)) => {
-                                        return Expression::Rational(Rational::new(a, b));
-                                    },
-                                    (&Expression::Float(a), &Expression::Integer(b)) => {
-                                        return Expression::Float(a / (b as f64));
-                                    },
-                                    (&Expression::Integer(a), &Expression::Float(b)) => {
-                                        return Expression::Float((a as f64) / b);
-                                    },
-                                    (&Expression::Float(a), &Expression::Float(b)) => {
-                                        return Expression::Float(a / b);
-                                    }
-                                    _ => {}
-                                }
-                            } else if body[1].is_numeric() {
-                                match &body[1] {
-                                    &Expression::Integer(a) => {
-                                        return Expression::List(
-                                            vec![
-                                            Expression::Symbol(String::from("__bin*")),
-                                            body[0].clone(),
-                                            Expression::Rational(Rational::new(1, a))
-                                            ]
-                                            );
-                                    },
-                                    &Expression::Float(a) => {
-                                        let inv = 1.0 / a;
-                                        return Expression::List(
-                                            vec![
-                                            Expression::Symbol(String::from("__bin*")),
-                                            body[0].clone(),
-                                            Expression::Float(inv)
-                                            ]
-                                            );
-                                    },
-                                    _ => {}
-                                }
-                            }
-                        },
-                        "div" => {
-                            if body[0].is_numeric() && body[1].is_numeric() {
-                                match (&body[0], &body[1]) {
-                                    (&Expression::Integer(a), &Expression::Integer(b)) => {
-                                        return Expression::Integer(a / b);
-                                    },
-                                    _ => {}
-                                }
-                            }
-                        },
-                        "pow" => {
-                            if body[0].is_numeric() && body[1].is_numeric() {
-                                match (&body[0], &body[1]) {
-                                    (&Expression::Integer(a), &Expression::Integer(b)) => {
-                                        return Expression::Integer(a.pow(b as u32));
-                                    },
-                                    _ => {}
-                                }
-                            }
-                        },
-                        "sqrt" => {
-                            if body[0].is_numeric() {
-                                match &body[0] {
-                                    &Expression::Integer(a) => {
-                                        return Expression::Float((a as f64).sqrt());
-                                    },
-                                    &Expression::Float(a) => {
-                                        return Expression::Float(a.sqrt());
-                                    },
-                                    _ => {}
-                                }
-                            }
-                        },
+    if let List(elems) = datum.clone() {
+        let mut body : Vec<Expression> = elems.into_iter().map(fold).collect();
+        let name = body.remove(0);
+        if let Symbol(ref s) = name.clone() {
+            match s.as_ref() {
+                "neg" => {
+                    match &body[0] {
+                        &Integer(a) => return Integer(-a),
+                        &Float(a) => return Float(-a),
                         _ => {}
                     }
-                    let mut res = vec![name];
-                    res.extend(body);
-                    Expression::List(res)
                 },
-                _ => {
-                    let mut res = vec![name];
-                    res.extend(body);
-                    Expression::List(res)
-                }
+                "__bin+" => {
+                    match (&body[0], &body[1]) {
+                        (&Integer(a), &Integer(b)) => return Integer(a + b),
+                        (&Float(a), &Integer(b)) => return Float(a + (b as f64)),
+                        (&Integer(a), &Float(b)) => return Float((a as f64) + b),
+                        (&Float(a), &Float(b)) => return Float(a + b),
+                        _ => {}
+                    }
+                },
+                "__bin-" => {
+                    match (&body[0], &body[1]) {
+                        (&Integer(a), &Integer(b)) => return Integer(a - b),
+                        (&Float(a), &Integer(b)) => return Float(a - (b as f64)),
+                        (&Integer(a), &Float(b)) => return Float((a as f64) - b),
+                        (&Float(a), &Float(b)) => return Float(a - b),
+                        _ => {}
+                    }
+                },
+                "__bin*" => {
+                    match (&body[0], &body[1]) {
+                        (&Integer(a), &Integer(b)) => return Integer(a * b),
+                        (&Float(a), &Integer(b)) => return Float(a * (b as f64)),
+                        (&Integer(a), &Float(b)) => return Float((a as f64) * b),
+                        (&Float(a), &Float(b)) => return Float(a * b),
+                        _ => {}
+                    }
+                },
+                "__bin/" => {
+                    if body[0].is_numeric() && body[1].is_numeric() {
+                        match (&body[0], &body[1]) {
+                            (&Integer(a), &Integer(b)) => {
+                                return Rational(numbers::Rational::new(a, b));
+                            },
+                            (&Float(a), &Integer(b)) => return Float(a / (b as f64)),
+                            (&Integer(a), &Float(b)) => return Float((a as f64) / b),
+                            (&Float(a), &Float(b)) => return Float(a / b),
+                            _ => {}
+                        }
+                    } else if body[1].is_numeric() {
+                        match &body[1] {
+                            &Integer(a) => {
+                                return List(vec![
+                                    Symbol(String::from("__bin*")),
+                                    body[0].clone(),
+                                    Rational(numbers::Rational::new(1, a))
+                                ]);
+                            },
+                            &Float(a) => {
+                                let inv = 1.0 / a;
+                                return List(vec![
+                                    Symbol(String::from("__bin*")),
+                                    body[0].clone(),
+                                    Float(inv)
+                                ]);
+                            },
+                            _ => {}
+                        }
+                    }
+                },
+                "div" => {
+                    match (&body[0], &body[1]) {
+                        (&Integer(a), &Integer(b)) => return Integer(a / b),
+                        _ => {}
+                    }
+                },
+                "pow" => {
+                    match (&body[0], &body[1]) {
+                        (&Integer(a), &Integer(b)) => return Integer(a.pow(b as u32)),
+                        _ => {}
+                    }
+                },
+                "sqrt" => {
+                    match &body[0] {
+                        &Integer(a) => return Float((a as f64).sqrt()),
+                        &Float(a) => return Float(a.sqrt()),
+                        _ => {}
+                    }
+                },
+                _ => {}
             }
-        },
-        other => other
+        }
     }
+
+    // If no folding is possible, return the original datum
+    datum
 }
