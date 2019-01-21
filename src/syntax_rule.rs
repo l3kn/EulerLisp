fcb01f24bc5cf00409265f42ec7ee5468672b81d
Leon Rische <leon.rische@me.com>
2019-01-21 21:24:43 +0100

    Fix handling of literals in syntax rules

--- src/syntax_rule.rs
+++ src/syntax_rule.rs
@@ -17,9 +17,9 @@ pub struct SyntaxRule {
 pub struct Rule(Pattern, Template);
 
 impl Rule {
-    pub fn parse(expr: Expression) -> Rule {
+    pub fn parse(expr: Expression, literals: &Vec<String>) -> Rule {
         let rule = expr.as_list().unwrap();
-        let pattern = Pattern::parse(rule[0].clone());
+        let pattern = Pattern::parse(rule[0].clone(), literals);
         let template = Template::parse(rule[1].clone());
 
         Rule(pattern, template)
@@ -29,9 +29,16 @@ impl Rule {
 impl SyntaxRule {
     pub fn parse(name: String, literals: Vec<Expression>, rules: Vec<Expression>) -> SyntaxRule {
         let literals = literals.iter().map(|l| l.as_symbol().unwrap()).collect();
-        let rules = rules.into_iter().map(Rule::parse).collect();
+        let rules = rules
+            .into_iter()
+            .map(|r| Rule::parse(r, &literals))
+            .collect();
 
-        SyntaxRule { name, literals, rules }
+        SyntaxRule {
+            name,
+            literals,
+            rules,
+        }
     }
 
     pub fn apply(&self, mut datums: Vec<Expression>) -> Option<Expression> {
@@ -61,7 +68,7 @@ impl SyntaxRule {
         match *pattern {
             Pattern::Ident(ref name) => {
                 if let Expression::Symbol(ref s) = datum {
-                    if *s == self.name || self.literals.contains(&s) {
+                    if *s == self.name {
                         true
                     } else {
                         bindings.insert(name.clone(), datum.clone());
@@ -70,6 +77,13 @@ impl SyntaxRule {
                 } else {
                     bindings.insert(name.clone(), datum.clone());
                     true
+                }
+            }
+            Pattern::Literal(ref name) => {
+                if let Expression::Symbol(ref s) = datum {
+                    s == name
+                } else {
+                    false
                 }
             }
             Pattern::Constant(ref c) => datum == *c,
@@ -132,6 +146,7 @@ impl SyntaxRule {
 pub enum Pattern {
     Ident(String),
     Constant(Expression),
+    Literal(String),
     // (<pattern> ...)
     List(Vec<Pattern>),
     // TODO: (<pattern> <pattern> ... . <pattern>)
@@ -148,32 +163,44 @@ fn is_ellipsis(datum: Expression) -> bool {
 }
 
 impl Pattern {
-    pub fn parse(expr: Expression) -> Pattern {
+    pub fn parse(expr: Expression, literals: &Vec<String>) -> Pattern {
         match expr {
             Expression::List(mut elems) => {
                 let last = elems[elems.len() - 1].clone();
                 if is_ellipsis(last) {
                     elems.pop();
-                    let rest = Pattern::parse(elems.pop().unwrap());
+                    let rest = Pattern::parse(elems.pop().unwrap(), literals);
                     Pattern::ListWithRest(
-                        elems.into_iter().map(Pattern::parse).collect(),
+                        elems
+                            .into_iter()
+                            .map(|e| Pattern::parse(e, literals))
+                            .collect(),
                         Box::new(rest),
                     )
                 } else {
-                    Pattern::List(elems.into_iter().map(Pattern::parse).collect())
+                    Pattern::List(
+                        elems
+                            .into_iter()
+                            .map(|e| Pattern::parse(e, literals))
+                            .collect(),
+                    )
                 }
             }
             Expression::Nil => Pattern::List(vec![]),
-            Expression::Symbol(s) => Pattern::Ident(s),
+            Expression::Symbol(s) => {
+                if literals.contains(&s) {
+                    Pattern::Literal(s)
+                } else {
+                    Pattern::Ident(s)
+                }
+            }
             other => Pattern::Constant(other),
         }
     }
 
     pub fn keys(&self) -> Vec<String> {
         match *self {
-            Pattern::List(ref elems) => {
-                elems.iter().flat_map(|e| e.keys()).collect()
-            }
+            Pattern::List(ref elems) => elems.iter().flat_map(|e| e.keys()).collect(),
             Pattern::ListWithRest(ref elems, ref rest) => {
                 let mut res: Vec<String> = elems.iter().flat_map(|e| e.keys()).collect();
                 res.append(&mut rest.keys());
@@ -181,6 +208,7 @@ impl Pattern {
             }
             Pattern::Ident(ref key) => vec![key.clone()],
             Pattern::Constant(_) => vec![],
+            Pattern::Literal(_) => vec![],
         }
     }
 }
@@ -243,18 +271,16 @@ impl Template {
                 for e in es.iter() {
                     match *e {
                         Element::Normal(ref t) => res.push(t.apply(bindings)),
-                        Element::Ellipsed(ref t) => {
-                            match t.apply(bindings) {
-                                Expression::List(ref mut inner) => res.append(inner),
-                                Expression::Nil => {},
-                                other => panic!(
-                                    "macro templates `<identifier> ...`/
+                        Element::Ellipsed(ref t) => match t.apply(bindings) {
+                            Expression::List(ref mut inner) => res.append(inner),
+                            Expression::Nil => {}
+                            other => panic!(
+                                "macro templates `<identifier> ...`/
                                             only work if binding is list,/
                                             not in {}",
-                                    other
-                                ),
-                            }
-                        }
+                                other
+                            ),
+                        },
                     }
                 }
                 Expression::List(res)
